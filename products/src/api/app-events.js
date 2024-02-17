fb985b644fce0881f17db3b494d9770e03497081
yigittin <i.yigittin@hotmail.com>
2024-02-17 22:54:16 +0300

    Products service integrated

---
+++ products/src/api/app-events.js
@@ -0,0 +1,9 @@
+module.exports=(app)=>{
+    app.use('/app-events',async (req,res,next)=>{
+        const {payload}=req.body;
+        console.log("============= Shopping ================");
+        console.log(payload);
+
+        return res.status(200).json({message:'notified'});
+    })
+}
